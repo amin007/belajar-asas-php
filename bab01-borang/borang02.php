082beaee7c28286bb43c1b4846ff155fc6511175
Amin Ledang@Hospikal Kluang <helmi007@yahoo.com>
2018-05-12 15:03:02 +0800

    letak pre sebaris

--- bab01-borang/borang02.php
+++ bab01-borang/borang02.php
@@ -1,8 +1,6 @@
 <?php
 
-/*echo '$_POST:<pre>';
-print_r($_POST);
-echo '</pre>';//*/
+/*echo '$_POST:<pre>';print_r($_POST);echo '</pre>';//*/
 
 include 'DB_Pdo.php';
 include 'tatarajah.php';
@@ -16,13 +14,8 @@ $sql = 'SELECT * FROM biodata' . "\r"
 	 . 'WHERE email like "' . $email . '" ' . "\r"
 	 . 'AND password like "' . $password . '" ';
 
-echo '$sql:<pre>';
-print_r($sql);
-echo '</pre>';
-	 
-	 
+echo '$sql:<pre>';print_r($sql);echo '</pre>';
+
 $result = $dbPdo->selectAll($sql);
 
-echo '$result:<pre>';
-print_r($result);
-echo '</pre>';
+echo '$result:<pre>';print_r($result);echo '</pre>';
