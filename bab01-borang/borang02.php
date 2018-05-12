f017fffd9073f532a9a1410f53747067b89fc3c8
Amin Ledang@Hospikal Kluang <helmi007@yahoo.com>
2018-05-12 12:46:11 +0800

    access database

--- bab01-borang/borang02.php
+++ bab01-borang/borang02.php
@@ -1,5 +1,17 @@
 <?php
 
-echo '<pre>';
+echo '$_POST:<pre>';
 print_r($_POST);
+echo '</pre>';
+
+include 'DB_Pdo.php';
+include 'tatarajah.php';
+$dbPdo = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+
+# panggil sql
+$sql = 'SELECT * FROM biodata';
+$result = $dbPdo->selectAll($sql);
+
+echo '$result:<pre>';
+print_r($result);
 echo '</pre>';
