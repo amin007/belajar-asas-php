ca32e3a4e19695975dde542cf4889612a85215a4
Amin Ledang@Hospikal Kluang <helmi007@yahoo.com>
2018-05-12 15:00:56 +0800

    semak password sama dengan database

--- bab01-borang/borang02.php
+++ bab01-borang/borang02.php
@@ -1,15 +1,26 @@
 <?php
 
-echo '$_POST:<pre>';
+/*echo '$_POST:<pre>';
 print_r($_POST);
-echo '</pre>';
+echo '</pre>';//*/
 
 include 'DB_Pdo.php';
 include 'tatarajah.php';
 $dbPdo = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
 
+# istihar pembolehubah
+$email = $_POST['biodata']['email'];
+$password = $_POST['biodata']['password'];
 # panggil sql
-$sql = 'SELECT * FROM biodata';
+$sql = 'SELECT * FROM biodata' . "\r"
+	 . 'WHERE email like "' . $email . '" ' . "\r"
+	 . 'AND password like "' . $password . '" ';
+
+echo '$sql:<pre>';
+print_r($sql);
+echo '</pre>';
+	 
+	 
 $result = $dbPdo->selectAll($sql);
 
 echo '$result:<pre>';
