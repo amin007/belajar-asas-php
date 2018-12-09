2352b29dd65d630d6c3f3f5b24e0075f626f2820
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-09 10:33:16 +0800

    tukar pembolehubah

--- bab02-jomweb/borang_proses.php
+++ bab02-jomweb/borang_proses.php
@@ -3,21 +3,22 @@
 /*echo '$_POST:<pre>';print_r($_POST);echo '</pre>';//*/
 
 include 'DB_Pdo.php';
+include 'Sql.php';
 include 'tatarajah.php';
 $dbPdo = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
 
 # istihar pembolehubah
 $jadual = 'biodata';
-$nim = $_POST[$jadual]['nim'];
-$nama = $_POST[$jadual]['nama'];
-$alanat = $_POST[$jadual]['alamat'];
-$bandar = $_POST[$jadual]['bandar'];
-$kdprody = $_POST[$jadual]['kdprody'];
+$medan = '`nim`,`nama`,`alamat`,`bandar`,`kdprody`';
+$posmen['nim'] = $_POST[$jadual]['nim'];
+$posmen['nama'] = $_POST[$jadual]['nama'];
+$posmen['alamat'] = $_POST[$jadual]['alamat'];
+$posmen['bandar'] = $_POST[$jadual]['bandar'];
+$posmen['kdprody'] = $_POST[$jadual]['kdprody'];
 # panggil sql update
-$sql = $this->tanya->ubahSqlSimpan
-	//ubahSimpan
-	($posmen[$jadual], $jadual, $medanID);
-	$paparID = $jadual . '/' . $dataID;
+//$tanya = new Aplikasi\Kitab\Sql();
+$tanya = new Sql();
+$sql = $tanya->tambahSqlBanyakNilai($jadual, $medan, $posmen);
 
 echo '$sql:<pre>'; print_r($sql); echo '</pre>';
 
