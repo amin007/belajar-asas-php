37c74ee3db8f9c196df4638ed2d1e97535a5c387
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-09 11:15:44 +0800

    pindah kedudukan koding

--- bab02-jomweb/borang_proses.php
+++ bab02-jomweb/borang_proses.php
@@ -6,6 +6,7 @@ include 'DB_Pdo.php';
 include 'Sql.php';
 include 'tatarajah.php';
 $dbPdo = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+$tanya = new Aplikasi\Kitab\Sql;
 
 # istihar pembolehubah
 $jadual = 'biodata';
@@ -15,9 +16,8 @@ $posmen['nama'] = $_POST[$jadual]['nama'];
 $posmen['alamat'] = $_POST[$jadual]['alamat'];
 $posmen['bandar'] = $_POST[$jadual]['bandar'];
 $posmen['kdprody'] = $_POST[$jadual]['kdprody'];
+
 # panggil sql update
-//$tanya = new Aplikasi\Kitab\Sql();
-$tanya = new Sql();
 $sql = $tanya->tambahSqlBanyakNilai($jadual, $medan, $posmen);
 
 echo '$sql:<pre>'; print_r($sql); echo '</pre>';
