9064f3c1e960a64d93dd906533147cc4fbe1afe6
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-09 09:55:52 +0800

    contoh borang html

---
+++ bab02-jomweb/borang_masuk.php
@@ -0,0 +1,60 @@
+<!doctypehtml>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport"content="width=device-width,initial-scale=1,shrink-to-fit=no">
+<meta name="description"content="">
+<meta name="author"content="">
+<link rel="icon"href="../../../../favicon.ico">
+
+<title>FloatinglabelsexampleforBootstrap</title>
+
+<!--Bootstrap core CSS-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+<!--Custom styles for this template-->
+<link href="floating-labels.css" rel="stylesheet">
+</head>
+
+<body>
+<form class="form-signin" method="POST" action="borang_proses.php">
+<div class="text-centermb-4">
+<img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+<h1 class="h3mb-3font-weight-normal">Floatinglabels</h1>
+</div>
+
+<div class="form-label-group">
+	<label for="inputEmail">nim</label>
+	<input type="email" name="biodata[nim]" id="inputEmail" class="form-control" placeholder="nim">
+</div>
+
+<div class="form-label-group">
+	<label for="inputEmail">nama</label>
+	<input type="email" name="biodata[nama]" id="inputEmail" class="form-control" placeholder="nama">
+</div>
+
+<div class="form-label-group">
+	<label for="inputEmail">alamat</label>
+	<input type="email" name="biodata[alamat]" id="inputEmail" class="form-control" placeholder="alamat">
+</div>
+
+<div class="form-label-group">
+	<label for="inputEmail">bandar</label>
+	<input type="email" name="biodata[bandar]" id="inputEmail" class="form-control" placeholder="bandar">
+</div>
+
+<div class="form-label-group">
+	<label for="inputEmail">kdprody</label>
+	<input type="email" name="biodata[kdprody]" id="inputEmail" class="form-control" placeholder="kdprody">
+</div>
+
+<div class="checkboxmb-3">
+	<label><input type="checkbox" value="remember-me">Remember me</label>
+</div>
+
+<input type="submit" value="Poslaju" class="btnbtn-lgbtn-primarybtn-block">
+
+<p class="mt-5mb-3text-mutedtext-center">&copy;2017-2018</p>
+</form>
+</body>
+</html>
