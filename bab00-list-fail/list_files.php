3d49edbebdee963f7d991ea74af6b9e8180ebe17
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-10-25 12:02:18 +0800

    ubah paparan koding ikut pre/kbd/code/samp

--- bab00-list-fail/list_files.php
+++ bab00-list-fail/list_files.php
@@ -117,19 +117,23 @@
 	function paparan($web, $test)
 	{
 		# set pembolehubah utama
+		$p1 = 'pre';#https://www.w3schools.com/tags/tag_var.asp
+		$p2 = 'kbd';
+		$p3 = 'code';
+		$p4 = 'samp';
 		$kira = kiraPaparan();
 		$data = explode('|', $web);
 		$semak01 = ($kira[01] - strlen($data[1]) + 1);
 		$semak02 = ($kira[02] - strlen($data[2]) + 1);
 		$semak03 = ($kira[03] - strlen($data[3]) + 1);
 		# papar
-			$p = "\n<code>| " . $data[1];
+			$p = "\n<$p2>| " . $data[1];
 			for($i = 1; $i < $semak01; $i++): $p .= "&nbsp;"; endfor;
 			$p .=  "|" . $data[2];
 			for($i = 1; $i < $semak02; $i++): $p .= "&nbsp;"; endfor;
 			$p .= "|" . $data[3];
 			for($i = 1; $i < $semak03; $i++): $p .= "&nbsp;"; endfor;
-			$p .= "|</code><hr>";
+			$p .= "&nbsp;|</$p2><hr>";
 		#
 		return $p;
 	}
