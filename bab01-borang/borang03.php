33192fe83387a3ef97865e18c79c5b186ad8456c
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-12-21 15:06:07 +0800

    tambah bentuk input type setakat hari ini 21/12/2020

---
+++ bab01-borang/borang03.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1,, maximum-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Borang Bootstrap</title>
+<link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" type="text/css">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+<style type="text/css">
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:11px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#cccccc;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align: top;
+}
+table.excel tbody th { text-align:center; vertical-align: top; }
+table.excel tbody td { vertical-align:bottom; }
+table.excel tbody td
+{
+	padding: 0 3px; border: 1px solid #aaaaaa;
+	background:#ffffff;
+}
+</style>
+</head>
+<body>
+<!-- .muka - borang ketiga ---------------------------------------------------------------------------------------------- -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<label for="inputNotaKaki" class="col-sm-2 control-label">&nbsp;Text-info</label>
+	<div class="col-sm-6">
+		<h2><a target="_blank" href="https://www.w3schools.com/html/html_form_input_types.asp">
+		HTML Input Types</a></h2>
+		<table class="table table-bordered table-hover">
+		<tr><th>Bentuk</th><th>Code</th></tr>
+		<tr><td align="right"><input type="button" value="button"></td><td><code>&lt;input type="button"&gt;</code></td></tr>
+		<tr><td align="right"><input type="checkbox"></td><td><code>&lt;input type="checkbox"&gt;</code></td></tr>
+		<tr><td align="right"><input type="color"></td><td><code>&lt;input type="color"&gt;</code></td></tr>
+		<tr><td align="right"><input type="date"></td><td><code>&lt;input type="date"&gt;</code></td></tr>
+		<tr><td align="right"><input type="datetime-local"></td><td><code>&lt;input type="datetime-local"&gt;</code></td></tr>
+		<tr><td align="right"><input type="email"></td><td><code>&lt;input type="email"&gt;</code></td></tr>
+		<tr><td align="right"><input type="file"></td><td><code>&lt;input type="file"&gt;</code></td></tr>
+		<tr><td align="right"><input type="hidden"></td><td><code>&lt;input type="hidden"&gt;</code></td></tr>
+		<tr><td align="right"><input type="image"></td><td><code>&lt;input type="image"&gt;</code></td></tr>
+		<tr><td align="right"><input type="month"></td><td><code>&lt;input type="month"&gt;</code></td></tr>
+		<tr><td align="right"><input type="number"></td><td><code>&lt;input type="number"&gt;</code></td></tr>
+		<tr><td align="right"><input type="password"></td><td><code>&lt;input type="password"&gt;</code></td></tr>
+		<tr><td align="right"><input type="radio"></td><td><code>&lt;input type="radio"&gt;</code></td></tr>
+		<tr><td align="right"><input type="range"></td><td><code>&lt;input type="range"&gt;</code></td></tr>
+		<tr><td align="right"><input type="reset"></td><td><code>&lt;input type="reset"&gt;</code></td></tr>
+		<tr><td align="right"><input type="search"></td><td><code>&lt;input type="search"&gt;</code></td></tr>
+		<tr><td align="right"><input type="submit"></td><td><code>&lt;input type="submit"&gt;</code></td></tr>
+		<tr><td align="right"><input type="tel"></td><td><code>&lt;input type="tel"&gt;</code></td></tr>
+		<tr><td align="right"><input type="text"></td><td><code>&lt;input type="text"&gt;</code></td></tr>
+		<tr><td align="right"><input type="time"></td><td><code>&lt;input type="time"&gt;</code></td></tr>
+		<tr><td align="right"><input type="url"></td><td><code>&lt;input type="url"&gt;</code></td></tr>
+		<tr><td align="right"><input type="week"></td><td><code>&lt;input type="week"&gt;</code></td></tr>
+		</table>
+	</div>
+</div><!-- / class="form-group row" -->
+<!-- tamat - borang ketiga ---------------------------------------------------------------------------------------------- -->
+<!-- Footer
+=============================================================================================== -->
+<!-- footer class="footer">
+	<div class="container">
+		<span class="label label-info">
+		&copy; Hak Cipta Terperihara 2016. Theme Asal Bootstrap Twitter
+		</span>
+	</div>
+</footer -->
+
+<!-- khas untuk jquery dan js2 lain
+=============================================================================================== -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script -->
+</body>
+</html>
