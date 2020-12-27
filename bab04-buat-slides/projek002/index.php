7382df69ae53c86991804b197b38675cad034920
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-12-27 16:20:06 +0800

    tambah projek baru

---
+++ bab04-buat-slides/projek002/index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<title>Asas PHP</title>
+
+<link rel="stylesheet" href="dist/reset.css">
+<link rel="stylesheet" href="dist/reveal.css">
+<link rel="stylesheet" href="dist/theme/black.css" id="theme">
+
+<!-- Theme used for syntax highlighted code -->
+<link rel="stylesheet" href="plugin/highlight/monokai.css" id="highlight-theme">
+<link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div class="reveal"><div class="slides">
+<!-- ########################################################################################## -->
+<!-- ========================================================================================== -->
+<section>
+	<section>
+		<h3>Slide 1.0 - Pengenalan Kepada PHP</h3><br>
+		Apa Yang Anda Perlu Tahu<br>
+		<details>
+		<summary>Sebelum kita meneruskan, anda harus mempunyai pemahaman asas:</summary>
+			<ul>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JavaScript</li>
+			</ul>
+		</details>
+
+
+	</section>
+	<section>Slide 1.1 - Apa itu PHP?
+
+		<ul>
+		<li> PHP adalah singkatan dari "PHP: Hypertext Preprocessor" </li>
+		<li> PHP adalah bahasa skrip sumber terbuka yang banyak digunakan </li>
+		<li> Skrip PHP dijalankan di pelayan </li>
+		<li> PHP percuma untuk memuat turun dan menggunakan </li>
+		<li> PHP untuk people-help-people </li>
+		</ul>
+	</section>
+	<section>Slide 1.2 - Apa itu Fail PHP?
+
+		<ul>
+		<li> Fail PHP dapat berisi teks, HTML, CSS, JavaScript, dan kod PHP </li>
+		<li> Kod PHP dijalankan di pelayan, dan hasilnya dikembalikan ke penyemak imbas sebagai HTML biasa </li>
+		<li> Fail PHP mempunyai sambungan ".php" </li>
+		</ul>
+	</section>
+	<section>
+	Slide 1.3 - Pengenalan Kepada PHP
+	</section>
+	<section>
+	Slide 1.4 - Pengenalan Kepada PHP
+	</section>
+	<section>
+	Slide 1.5 - Pengenalan Kepada PHP
+	</section>
+</section>
+<!-- ========================================================================================== -->
+<section>Slide 2</section>
+<!-- ========================================================================================== -->
+<!-- penghabisan - mula -->
+<section>
+	<section><h2>SUDAH HABIS DAA</h2>
+		<hr/>
+			<ul>
+			<li><i class="fas fa-bookmark"></i>: www.amin007.org</li>
+			<li><i class="fas fa-envelope"></i>: al.amin007@yahoo.com</li>
+			<li><i class="fab fa-facebook"></i>: amin007.ledang</li>
+			<li><i class="fab fa-twitter"></i>: amin007_ledang</li>
+			<li><i class="fab fa-linkedin"></i>: linkedin.com/in/amin007ledang</li>
+			<li><i class="fab fa-github"></i>: amin007</li>
+			<li><i class="fab fa-instagram"></i>: aminledang</li>
+			<!-- li><i class="fab fa-skype"></i>: amin800507</li -->
+			<li><i class="fab fa-youtube"></i>: user/abdmuhaimin</li>
+			</ul>
+	</section>
+</section>
+<!-- penghabisan - tamat -->
+<!-- ========================================================================================== -->
+<!-- ########################################################################################## -->
+</div><!-- / class="slides" -->
+</div><!-- / class="reveal" -->
+
+<!--
+# masukkan js dan jquery di bawah ini ########################################################################################### -->
+<script src="dist/reveal.js"></script>
+<script src="plugin/notes/notes.js"></script>
+<script src="plugin/markdown/markdown.js"></script>
+<script src="plugin/highlight/highlight.js"></script>
+<script>
+// More info about initialization & config:
+// - https://revealjs.com/initialization/
+// - https://revealjs.com/config/
+Reveal.initialize({
+	hash: true,
+	// Learn about plugins: https://revealjs.com/plugins/
+	plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
+});
+</script>
+<!--
+# masukkan body html
+########################################################################################### -->
+</body>
+</html>
