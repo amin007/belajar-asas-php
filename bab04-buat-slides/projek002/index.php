87b4282e4fcb29bf5a5056ed87163ebba6ce60de
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-12-27 17:37:35 +0800

    ubahsuai kandungan

--- bab04-buat-slides/projek002/index.php
+++ bab04-buat-slides/projek002/index.php
@@ -13,30 +13,28 @@
 <!-- Theme used for syntax highlighted code -->
 <link rel="stylesheet" href="plugin/highlight/monokai.css" id="highlight-theme">
 <link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" type="text/css">
+<?php
+
+$fontSize = 'font-size: 30px';
+
+?>
 </head>
 <body>
-
 <div class="reveal"><div class="slides">
 <!-- ########################################################################################## -->
 <!-- ========================================================================================== -->
 <section>
-	<section>
-		<h3>Slide 1.0 - Pengenalan Kepada PHP</h3><br>
-		Apa Yang Anda Perlu Tahu<br>
-		<details>
-		<summary>Sebelum kita meneruskan, anda harus mempunyai pemahaman asas:</summary>
-			<ul>
-			<li>HTML</li>
-			<li>CSS</li>
-			<li>JavaScript</li>
-			</ul>
-		</details>
-
-
+	<section><h4>Slide 1.0</h4><hr/>
+		<p>Apa Yang Anda Perlu Tahu</p>
+		<ul style="<?php echo $fontSize ?>">
+		<li>HTML</li>
+		<li>CSS</li>
+		<li>JavaScript</li>
+		</ul>
 	</section>
-	<section>Slide 1.1 - Apa itu PHP?
+	<section><h4>Slide 1.1 - Apa itu PHP?</h4><hr/>
 
-		<ul>
+		<ul style="<?php echo $fontSize ?>">
 		<li> PHP adalah singkatan dari "PHP: Hypertext Preprocessor" </li>
 		<li> PHP adalah bahasa skrip sumber terbuka yang banyak digunakan </li>
 		<li> Skrip PHP dijalankan di pelayan </li>
@@ -44,31 +42,75 @@
 		<li> PHP untuk people-help-people </li>
 		</ul>
 	</section>
-	<section>Slide 1.2 - Apa itu Fail PHP?
+	<section><h4>Slide 1.2 - Apa itu Fail PHP?</h4><hr/>
 
-		<ul>
+		<ul style="<?php echo $fontSize ?>">
 		<li> Fail PHP dapat berisi teks, HTML, CSS, JavaScript, dan kod PHP </li>
 		<li> Kod PHP dijalankan di pelayan, dan hasilnya dikembalikan ke penyemak imbas sebagai HTML biasa </li>
 		<li> Fail PHP mempunyai sambungan ".php" </li>
 		</ul>
 	</section>
-	<section>
-	Slide 1.3 - Pengenalan Kepada PHP
+	<section><h4>Slide 1.3 - Apa yang Boleh Dilakukan PHP?</h4><hr/>
+		<ul style="<?php echo $fontSize ?>">
+		<li> PHP dapat menghasilkan kandungan halaman yang dinamik </li>
+		<li> PHP dapat membuat, membuka, membaca, menulis, menghapus, dan menutup fail di pelayan </li>
+		<li> PHP dapat mengumpulkan data borang </li>
+		<li> PHP boleh menghantar dan menerima kuki </li>
+		<li> PHP dapat menambah, menghapus, mengubah data dalam pangkalan data anda </li>
+		<li> PHP boleh digunakan untuk mengawal akses pengguna </li>
+		<li> PHP dapat menyulitkan data </li>
+		</ul>
+		<p style="<?php echo $fontSize ?>">Dengan PHP anda tidak terhad kepada output HTML. Anda boleh mengeluarkan gambar, fail PDF, dan juga filem Flash. Anda juga dapat mengeluarkan teks apa pun, seperti XHTML dan XML.</p>
 	</section>
-	<section>
-	Slide 1.4 - Pengenalan Kepada PHP
+	<section><h4>Slide 1.4 - Mengapa PHP?</h4><hr/>
+		<ul style="<?php echo $fontSize ?>">
+		<li> PHP berjalan di pelbagai platform (Windows, Linux, Unix, Mac OS X, dll.) </li>
+		<li> PHP serasi dengan hampir semua pelayan yang digunakan hari ini (Apache, IIS, dll.) </li>
+		<li> PHP menyokong pelbagai pangkalan data </li>
+		<li> PHP adalah percuma. Muat turunnya dari sumber PHP rasmi: www.php.net </li>
+		<li> PHP mudah dipelajari dan berjalan dengan cekap di sisi pelayan </li>
+		</ul>
 	</section>
-	<section>
-	Slide 1.5 - Pengenalan Kepada PHP
+	<section><h4>Slide 1.5 - Apa yang baru dalam PHP 7</h4><hr/>
+		<ul style="<?php echo $fontSize ?>">
+		<li> PHP 7 jauh lebih pantas daripada pelepasan stabil popular sebelumnya (PHP 5.6) </li>
+		<li> PHP 7 telah meningkatkan Pengendalian Ralat </li>
+		<li> PHP 7 menyokong Deklarasi Jenis yang lebih ketat untuk argumen fungsi </li>
+		<li> PHP 7 menyokong pengendali baru (seperti pengendali kapal angkasa: <code><=></code>) </li>
+		</ul>
 	</section>
 </section>
 <!-- ========================================================================================== -->
-<section>Slide 2</section>
+<section>
+	<section><h4>Slide 2.0 - Pasang perisian</h4><hr/>
+		xampp<br>laragon<br>
+	</section>
+	<section><h4>Slide 2.1</h4><hr/>
+	</section>
+</section>
+<!-- ========================================================================================== -->
+<section>
+	<section><h4>Slide 3.0 - koding asas php</h4><hr/>
+		<ul style="<?php echo $fontSize ?>">
+		<li> hantar data ke penyemak imbas web </li>
+		<li> menulis komen </li>
+		<li> pemboleh ubah </li>
+		<li> rentetan, nombor, pemalar </li>
+		<li> tanda petik tunggal vs dua kali ganda </li>
+		<li> membuat bentuk melekit </li>
+		<li> membuat dan memanggil fungsi anda </li>
+		<li> fungsi tarikh dan waktu </li>
+		<li> menggunakan sesi </li>
+		</ul>
+	
+	</section>
+	<section><h4>Slide 3.1</h4><hr/>
+	</section>
+</section>
 <!-- ========================================================================================== -->
 <!-- penghabisan - mula -->
 <section>
-	<section><h2>SUDAH HABIS DAA</h2>
-		<hr/>
+	<section><h2>SUDAH HABIS DAA</h2><hr/>
 			<ul>
 			<li><i class="fas fa-bookmark"></i>: www.amin007.org</li>
 			<li><i class="fas fa-envelope"></i>: al.amin007@yahoo.com</li>
@@ -80,6 +122,12 @@
 			<!-- li><i class="fab fa-skype"></i>: amin800507</li -->
 			<li><i class="fab fa-youtube"></i>: user/abdmuhaimin</li>
 			</ul>
+	</section>
+	<section><h2>Cetak dalam bentuk pdf</h2><hr/>
+		<a target="_blank" href="./?print-pdf">
+			<i class="fas fa-print"></i>
+			<i class="far fa-file-pdf"></i>
+		</a>
 	</section>
 </section>
 <!-- penghabisan - tamat -->
@@ -109,3 +157,15 @@ Reveal.initialize({
 ########################################################################################### -->
 </body>
 </html>
+<?php
+/*
+		<details>
+		<summary>Sebelum kita meneruskan, anda harus mempunyai pemahaman asas:</summary>
+			<ul>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JavaScript</li>
+			</ul>
+		</details>
+*/
+?>
