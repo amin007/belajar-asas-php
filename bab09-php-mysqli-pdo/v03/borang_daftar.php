555436e1373df97f148d41787acc9772b5840865
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2021-02-28 09:44:10 +0800

    susun balik koding

--- bab09-php-mysqli-pdo/v03/borang_daftar.php
+++ bab09-php-mysqli-pdo/v03/borang_daftar.php
@@ -5,39 +5,39 @@
 		NG		DB					BP						TK																NOIR			NOAIR
 </td></tr>
 <tr><td>
-Bahagian A (Butir-butir Ahli Isi Rumah)
-	<table><tr><td>
+	Bahagian A (Butir-butir Ahli Isi Rumah)
+	<table width="100%"><tr><td width="50%">
 	<br>A1.	Nama :		{nama}:
 	<br>A2.	No Kad Pengenalan :
 	<br>A3.	No telefon:
 	<br>A4.	Umur :					4		9			015 - 200
 	<br>A5.	Jantina :		1	Lelaki
 						2	Perempuan
-</td>
-<td>
-<ul>
-<li>A6.	Kumpulan etnik</li>
-<li>1	Bumiputera</li>
-<li>2 	Cina</li>
-<li>3	India</li>
-<li>4	Lain-Lain</li>
-<li>5	Bukan Warganegara</li>
-</ul>
+	</td width="50%">
+	<td>
+	<ul>
+	<li>A6.	Kumpulan etnik</li>
+	<li>1	Bumiputera</li>
+	<li>2 	Cina</li>
+	<li>3	India</li>
+	<li>4	Lain-Lain</li>
+	<li>5	Bukan Warganegara</li>
+	</ul>
 
-<ul>
-<li>A7.	Taraf Pendidikan :</li>
-<li>1 Rendah</li>
-<li>2	Menengah</li>
-<li>3	Tertiari</li>
-<li>4	Tiada pendidikan rasmi</li>
-</ul>
-</td></tr>
-<table>
+	<ul>
+	<li>A7.	Taraf Pendidikan :</li>
+	<li>1	Rendah</li>
+	<li>2	Menengah</li>
+	<li>3	Tertiari</li>
+	<li>4	Tiada pendidikan rasmi</li>
+	</ul>
+	</td></tr>
+	<table>
 </td></tr>
 <tr><td>
 	Bahagian B (Pekerjaan Semasa) - PERLU DIISI OLEH AHLI YANG BERUMUR 15 TAHUN KEATAS
 </td></tr>
-<tr><td>
+<tr><td width="50%">
 	S1.	Adakah anda bekerja sekurang-kurangnya
 		SEJAM (Minggu Rujukan) kerana upah atau
 		keuntungan keluarga? (Termasuk yang bekerja sendiri).
@@ -83,14 +83,14 @@ Bahagian A (Butir-butir Ahli Isi Rumah)
 		<li>8	Kehilangan Pekerjaan ( PKP 2.0)</li>
 
 		S12.	Berapa lamakah anda sudah mencari kerja?
-		1	Kurang dari 3 bulan</li>
+		<li>1	Kurang dari 3 bulan</li>
 		<li>2	3 bulan–kurang dari 6 bulan</li>
 		<li>3	6 bulan–kurang dari 1 tahun</li>
 		<li>4	1–3 tahun</li>
 		<li>5	Lebih daripada 3 tahun</li>
 		<li>
 	</td>
-	<td>
+	<td width="50%">
 		S7.	Adakah anda mencari kerja pada minggu rujukan?
 		<li>1	Ya	Terus ke S11 dan seterusnya</li>
 		<li>2	Tidak</li>
@@ -99,110 +99,160 @@ Bahagian A (Butir-butir Ahli Isi Rumah)
 		<li>1	Ya</li>
 		<li>2	Tidak</li>
 
-		S8.	Kenapa anda tidak mencari kerja pada minggu rujukan?
-		01	Bersekolah/masih belajar/program latihan
-		02	Kerja rumah/tanggungjawab keluarga/komuniti
-		03	Percaya tiada kerja/tiada kerja yang sesuai
-		04	Keadaan cuaca
-		05	Sakit/bersalin atau keguguran
-		06	Akan memulai menjawat kerja baru
-		07	Akan melanjutkan pelajaran
-		08	Hilang upaya/keilatan
-		09	Tiada minat/baru tamat belajar
-		10	Menunggu keputusan permohonan pekerjaan/
-			telah mencari kerja terdahulu dari minggu lalu
-		11	Tiada kelayakan/kurang kemahiran
-		12	Bersara
-		13	Lanjut usia
+		S8.	Kenapa anda tidak mencari kerja pada minggu rujukan?</li>
+		<li>01	Bersekolah/masih belajar/program latihan</li>
+		<li>02	Kerja rumah/tanggungjawab keluarga/komuniti</li>
+		<li>03	Percaya tiada kerja/tiada kerja yang sesuai</li>
+		<li>04	Keadaan cuaca</li>
+		<li>05	Sakit/bersalin atau keguguran</li>
+		<li>06	Akan memulai menjawat kerja baru</li>
+		<li>07	Akan melanjutkan pelajaran</li>
+		<li>08	Hilang upaya/keilatan</li>
+		<li>09	Tiada minat/baru tamat belajar</li>
+		<li>10	Menunggu keputusan permohonan pekerjaan/
+			telah mencari kerja terdahulu dari minggu lalu</li>
+		<li>11	Tiada kelayakan/kurang kemahiran</li>
+		<li>12	Bersara</li>
+		<li>13	Lanjut usia</li>
 
 		S9.	Adakah anda ingin bekerja jika peluang pekerjaan atau perniagaan tersedia?
 		<li>1	Ya</li>
 		<li>2	Tidak</li>
 
 		S10.	Jika peluang pekerjaan atau perniagaan tersedia,adakah anda...
-		1	Boleh mula bekerja dalam tempoh 7 hari yang lalu
-		2	Boleh mula bekerja dalam tempoh dua minggu akan datang
-		3	Belum bersedia untuk bekerja
+		<li>1	Boleh mula bekerja dalam tempoh 7 hari yang lalu</li>
+		<li>2	Boleh mula bekerja dalam tempoh dua minggu akan datang</li>
+		<li>3	Belum bersedia untuk bekerja</li>
 
 		S11.	Apakah langkah-langkah yang telah anda ambil untuk mendapatkan kerja pada minggu rujukan?
-		1	Jabatan Tenaga Kerja/Agensi Pekerjaan
-		2	Menjawab iklan/mengiklan
-		3	Menghantar surat/emel permohonan
-		4	Beritahu sahabat/saudara mara
-		5	Menghadiri karnival kerjaya/keusahawanan
+		<li>1	Jabatan Tenaga Kerja/Agensi Pekerjaan</li>
+		<li>2	Menjawab iklan/mengiklan</li>
+		<li>3	Menghantar surat/emel permohonan</li>
+		<li>4	Beritahu sahabat/saudara mara</li>
+		<li>5	Menghadiri karnival kerjaya/keusahawanan</li>
 
 		S13.	Apakah anda buat pada minggu rujukan?
-		1	Masih belajar
-		2	Baru berhenti sekolah
-		3	Kerja rumah/tanggungjawab keluarga/komuniti
-		4	Hilang upaya/keilatan
-		5	Bukan musim/luar musim
-		6	Henti kerja sementara
-		7	Bersara
-		8	Lanjut usia
+		<li>1	Masih belajar</li>
+		<li>2	Baru berhenti sekolah</li>
+		<li>3	Kerja rumah/tanggungjawab keluarga/komuniti</li>
+		<li>4	Hilang upaya/keilatan</li>
+		<li>5	Bukan musim/luar musim</li>
+		<li>6	Henti kerja sementara</li>
+		<li>7	Bersara</li>
+		<li>8	Lanjut usia</li>
+
 	</td>
 </tr>
 <tr><td>
-	Bahagian C (Kajian kesan COVID-19/ PKP 2.0) - PERLU DIISI OLEH AHLI YANG BERUMUR 15 TAHUN KEATAS<
-/td></tr>
-<tr><td>
-	P1.	Jika anda bekerja/ pernah bekerja, sila nyatakan maklumat pekerjaan dalam tempoh berikut :
-	a)	Tarikh terakhir pekerjaan anda? (Jika anda telah berhenti kerja)
-							0		4				2		0			2
-			Hari				Bulan Tahun
-		b)	"i) Taraf pekerjaan (sebelum PKP 2.0) /
-iv) Taraf pekerjaan (semasa)"																ii) Pekerjaan						iii) Industri							v) Pekerjaan 					vi) Industri
-																			Sebelum PKP 2.0	Semasa
-			1	Majikan
-			2	Pekerja Kerajaan
-			3	Pekerja Swasta
-			4	Bekerja Sendiri
-			5	Pekerja Keluarga Tanpa Gaji
-			6	Perantis atau Pelatih Berbayar
-
-			c)	Bilangan jam bekerja sehari																(i)		0		8									(ii)																		
-		d)	Adakah pekerjaan tersebut sesuai 																(i)	1 Ya						2 Tidak						(ii)	1 Ya				2 Tidak													
-			dengan siji/kemahiran yang dimiliki ?
-		e)	Gaji/ Upah/ Faedah (RM)																(i)	2000												(ii)
-		f)	Di manakah anda menjalankan pekerjaan pada minggu rujukan ? (Bagi Ahli yang bekerja - Boleh pilih lebih dari satu)
-			1	Di tempat kediaman anda (BDR)																					5	Tapak Pembinaan
-			2	Ruang bekerja di tempat kediaman anda																			6	Pasar/ Bazar/ Gerai
-			3	Kilang/ Pejabat/ Bengkel/ Kiosk																					7	Ladang/ Kebun / Laut (yang berkaitan pertanian)
-			4	Tempat kediaman majikan																							8	Tiada tempat yang tetap/ khusus
+	Bahagian C (Kajian kesan COVID-19/ PKP 2.0) - PERLU DIISI OLEH AHLI YANG BERUMUR 15 TAHUN KEATAS
 </td></tr>
 <tr><td>
-	P2.	Pernahkah anda menolak tawaran kerja sebelum ini?  Jika Ya, sila nyatakan sebab anda berbuat demikian.
-		 (Boleh pilih lebih dari satu)
-		1	Telah mendapat pekerjaan lain																						4	Waktu kerja / Waktu Perjalanan
-		2	Faedah yang ditawarkan rendah																						5	Jarak ke Tempat Kerja
-		3	Kadar gaji tidak setaraf dengan sijil yang dimiliki																	6	Budaya dan Fleksibiliti Tempat Kerja
+	<table width="100%">
+	<tr><td width="5%" valign="top">P1.</td>
+	<td>
+		<table border="1">
+		<tr><td colspan="10">
+		Jika anda bekerja/ pernah bekerja, sila nyatakan maklumat pekerjaan dalam tempoh berikut :
+		</td></tr>
+		<tr><td>a)</td><td colspan="6">
+			<table>
+			<tr><td colspan="5">Tarikh terakhir pekerjaan anda? (Jika anda telah berhenti kerja)</td></tr>
+			<tr><td>[][]</td><td rowspan="2">-</td><td>[][]</td><td rowspan="2">-</td><td>[][][][]</td></tr>
+			<tr><td>Hari</td><td>Bulan</td><td colspan="1">Tahun</td></tr>
+			</table>
+		</td></tr>
+		<tr>
+		<td rowspan="8" valign="top">b)</td>
+		<td>i) Taraf pekerjaan (sebelum PKP 2.0)</td>
+		<td>ii) Pekerjaan</td><td>iii) Industri</td>
+		<td>iv) Taraf pekerjaan (semasa)</td>
+		<td>v) Pekerjaan</td><td>vi) Industri</td>
+		</tr>
+		<tr><td>&nbsp;</td><td colspan="2">Sebelum PKP 2.0</td><td>&nbsp;</td><td colspan="2">Semasa</td></tr>
+		<tr><td>1	Majikan</td><td>&nbsp;</td><td>&nbsp;</td><td>1	Majikan</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+		<tr><td>2	Pekerja Kerajaan</td><td>&nbsp;</td><td>&nbsp;</td><td>2	Pekerja Kerajaan</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+		<tr><td>3	Pekerja Swasta</td><td>&nbsp;</td><td>&nbsp;</td><td>3	Pekerja Swasta</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+		<tr><td>4	Bekerja Sendiri</td><td>&nbsp;</td><td>&nbsp;</td><td>4	Bekerja Sendiri</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+		<tr><td>5	Pekerja Keluarga Tanpa Gaji</td><td>&nbsp;</td><td>&nbsp;</td><td>5	Pekerja Keluarga Tanpa Gaji</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+		<tr><td>6	Perantis atau Pelatih Berbayar</td><td>&nbsp;</td><td>&nbsp;</td><td>6	Perantis atau Pelatih Berbayar</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+		<tr><td>c)</td><td>Bilangan jam bekerja sehari</td><td colspan="2">(i)		0		8</td><td>&nbsp;</td><td colspan="2">(ii)</td></r>
+		<tr><td>d)</td><td>Adakah pekerjaan tersebut sesuai<br>dengan siji/kemahiran yang dimiliki ?</td>
+			<td colspan="2">(i)	1 Ya	2 Tidak</td><td>&nbsp;</td><td colspan="2">(ii)	1 Ya				2 Tidak</td></tr>
+		<tr><td>e)</td><td>Gaji/ Upah/ Faedah (RM)</td><td colspan="2">(i)	2000</td><td>&nbsp;</td><td colspan="2">(ii)</td></tr>
+		<tr><td>f)</td><td colspan="6">Di manakah anda menjalankan pekerjaan pada minggu rujukan ? (Bagi Ahli yang bekerja - Boleh pilih lebih dari satu)</td></tr>
+		<tr><td>&nbsp;</td><td colspan="3">1	Di tempat kediaman anda (BDR)</td><td colspan="3">5	Tapak Pembinaan</td></tr>
+		<tr><td>&nbsp;</td><td colspan="3">2	Ruang bekerja di tempat kediaman anda</td><td colspan="3">6	Pasar/ Bazar/ Gerai</td></tr>
+		<tr><td>&nbsp;</td><td colspan="3">3	Kilang/ Pejabat/ Bengkel/ Kiosk</td><td colspan="3">7	Ladang/ Kebun / Laut (yang berkaitan pertanian)</td></tr>
+		<tr><td>&nbsp;</td><td colspan="3">4	Tempat kediaman majikan</td><td colspan="3">8	Tiada tempat yang tetap/ khusus</td></tr>
+		</table>
+	</td></tr>
+	</table>
+	</td></tr>
+	<tr><td>
+		<table width="100%">
+		<tr><td width="5%" valign="top">P2.</td>
+		<td>
+			Pernahkah anda menolak tawaran kerja sebelum ini?  Jika Ya, sila nyatakan sebab anda berbuat demikian.<br>
+			(Boleh pilih lebih dari satu)<br>
+			<table border="1">
+			<tr><td>1	Telah mendapat pekerjaan lain</td><td>4	Waktu kerja / Waktu Perjalanan</td></tr>
+			<tr><td>2	Faedah yang ditawarkan rendah</td><td>5	Jarak ke Tempat Kerja</td></tr>
+			<tr><td>3	Kadar gaji tidak setaraf dengan sijil yang dimiliki</td><td>6	Budaya dan Fleksibiliti Tempat Kerja</td></tr>
+			</table>
+		</td><tr>
+		</table>
+	</td></tr>
+	<tr><td>
+		<table width="100%">
+		<tr><td width="5%" valign="top">P3.</td>
+		<td>
+			Apakah kesan/ impak penularan COVID-19 (PKP 2.0) kepada anda? (Boleh pilih lebih dari satu)
+			<table border="1">
+			<tr><td>1	Kehilangan Pekerjaan</td><td>5	Peningkatan Kos Perbelanjaan Kesihatan</td></tr>
+			<tr><td>2	Hilang Sumber Pendapatan</td><td>6	Kurang Aktiviti Sukan dan Riadah</td></tr>
+			<tr><td>3	Tutup Perniagaan/ Premis</td><td>7	Kesukaran Pergerakan Aktivi Harian</td></tr>
+			<tr><td>4	Peningkatan Kos Perbelanjaan Makanan</td><td>8	Kurang Aktiviti Percutian/ Pelancongan</td></tr>
+			</table>
+		</td></tr>
+		</table>
+	</td></tr>
+	<tr><td>
+		<table width="100%">
+		<tr><td width="5%" valign="top">P4.</td>
+		<td>
+			Adakah anda menerima sebarang insentif/ bantuan daripada kerajaan?
+			<li>1	Ya</li>
+			<li>2 Tidak</li>
+		</td></tr>
+		</table>
+	</td></tr>
+	<tr><td>
+		<table width="100%">
+		<tr><td width="5%" valign="top">P5.</td>
+		<td>
+		Jika Ya, sila nyatakan pilihan berikut (Boleh pilih lebih daripada satu)
+			<table border="1">
+			<tr><td>1	Bantuan Sara Hidup (BSH)</td><td>6	Penangguhan bayaran pinjaman PTPTN / PTPK</td></tr>
+			<tr><td>2	Bantuan Prihatin Nasional (BPN) /e-hailing/ pemandu teksi</td><td>7	Geran Subsidi Upah</td></tr>
+			<tr><td>3	Bantuan Khas Penjawat Awam/Pesara</td><td>8	Pengeluaran Akaun i-Sinar /i-Lestari</td></tr>
+			<tr><td>4	Bantuan Orang Kelainan Upaya (OKU)</td><td>9	Bantuan Khas PERMAI 2021</td></tr>
+			<tr><td>5	Moratorium pinjaman
+			</table>
+		</td></tr>
+		</table>
+	</td></tr>
+	<tr><td>
+		<table width="100%">
+		<tr><td width="5%" valign="top">PKP 2.0:</td>
+		<td>
+		<li>13 Januari - 4 Februari 2021 bagi seluruh negeri Pulau Pinang, Selangor, Wilayah Persekutuan (Kuala Lumpur, Putrajaya dan Labuan), Melaka, Johor dan Sabah.</li>
+		<li>16 Januari - 29 Januari 2021 bagi daerah Sibu, Selangau dan Kanowit, Sarawak.</li>
+		<li>16 Januari - 4 Februari 2021 bagi negeri Kelantan.</li>
+		<li>19 Januari - 1 Februari 2021 bagi daerah Seremban dan Port Dickson, Negeri Sembilan.</li>
+		<li>22 Januari - 4 Februari 2021 bagi seluruh negeri Perlis, Kedah, Terengganu, Perak, Pahang, Kelantan dan Negeri Sembilan.</li>
+		<li>5 Februari - 18 Februari 2021 bagi seluruh negeri kecuali Sarawak.</li>
+		</li>
+		</td></tr>
+		</table>
 </td></tr>
-<tr><td>
-	P3.	"Apakah kesan/ impak penularan COVID-19 (PKP 2.0) kepada anda? (Boleh pilih lebih dari satu)"
-	1	Kehilangan Pekerjaan																									5	Peningkatan Kos Perbelanjaan Kesihatan
-		2	Hilang Sumber Pendapatan																							6	Kurang Aktiviti Sukan dan Riadah
-		3	Tutup Perniagaan/ Premis																							7	Kesukaran Pergerakan Aktivi Harian
-		4	"Peningkatan Kos Perbelanjaan Makanan
-"																								8	"Kurang Aktiviti Percutian/ Pelancongan
-</td></tr>
-<tr><td>
-	P4.	Adakah anda menerima sebarang insentif/ bantuan daripada kerajaan?
-		<li>1	Ya
-		<li>2 Tidak</li>
-</td></tr>
-<tr><td>
-	P5.	Jika Ya, sila nyatakan pilihan berikut (Boleh pilih lebih daripada satu)
-		1	Bantuan Sara Hidup (BSH)																							6	Penangguhan bayaran pinjaman PTPTN / PTPK
-		2	Bantuan Prihatin Nasional (BPN) /e-hailing/ pemandu teksi															7	Geran Subsidi Upah
-		3	Bantuan Khas Penjawat Awam/Pesara 																					8	Pengeluaran Akaun i-Sinar /i-Lestari
-		4	Bantuan Orang Kelainan Upaya (OKU) 																					9	Bantuan Khas PERMAI 2021
-		5	Moratorium pinjaman
-</td></tr>
-<tr><td>
-PKP 2.0:		13 Januari - 4 Februari 2021 bagi seluruh negeri Pulau Pinang, Selangor, Wilayah Persekutuan (Kuala Lumpur, Putrajaya dan Labuan), Melaka, Johor dan Sabah.
-				16 Januari - 29 Januari 2021 bagi daerah Sibu, Selangau dan Kanowit, Sarawak.
-				16 Januari - 4 Februari 2021 bagi negeri Kelantan.
-				19 Januari - 1 Februari 2021 bagi daerah Seremban dan Port Dickson, Negeri Sembilan.
-				22 Januari - 4 Februari 2021 bagi seluruh negeri Perlis, Kedah, Terengganu, Perak, Pahang, Kelantan dan Negeri Sembilan.
-				5 Februari - 18 Februari 2021 bagi seluruh negeri kecuali Sarawak.</td></tr>
 </table>
