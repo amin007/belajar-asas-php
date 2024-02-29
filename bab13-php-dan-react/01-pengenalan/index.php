4a7c5a68eeab8fb1e45d245c3142dabacdf3a5d4
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2024-02-29 12:14:37 +0800

    tambah data baru

--- bab13-php-dan-react/01-pengenalan/index.php
+++ bab13-php-dan-react/01-pengenalan/index.php
@@ -11,7 +11,7 @@
 <!-- untuk sumber react -->
 <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
 <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
-<!-- untuk sumber jsx -->
+<!-- untuk sumber jsx.  Don't use this in production: -->
 <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
 <script type="text/jsx">
 const container = document.getElementById('akar');
@@ -23,5 +23,15 @@ akar.render(
 	</>
 );
 </script>
+<!-- ##############################################################################################
+Note: this page is a great way to try React but it's not suitable for production.
+It slowly compiles JSX with Babel in the browser and uses a large development build of React.
+
+Read this page for starting a new React project with JSX:
+https://react.dev/learn/start-a-new-react-project
+
+Read this page for adding React with JSX to an existing project:
+https://react.dev/learn/add-react-to-an-existing-project
+############################################################################################### -->
 </body>
 </html>
